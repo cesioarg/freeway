b50380224e7a5818802360b5c5b186ade791a679
Leandro Inocencio <cesio.arg@gmail.com>
2020-02-10 16:43:22 -0500

    first clean up

--- freeway.py
+++ freeway.py
@@ -10,83 +10,64 @@ _PLACEHOLDER_REGEX = re.compile('{(.+?)}')
 _FIELDPATTERN_REGEX = re.compile('({.+?})')
 _FIELDREPLACE = r'(?P<%s>[:a-zA-Z0-9_-]*)'
 
-rules = {}
-convertionTable = {}
+
+def loadRulesFromFile(filename):
+    print("Loading JSON file:", filename)
+
+    with open(filename, 'r') as jsonf:
+        jsonData = json.loads(jsonf.read())
+        return jsonData
 
 
-class JSONReadError(Exception):
-    pass
+rulesfile = os.environ.get('RULESFILE', None)
 
-
-class InvalidFilePath(Exception):
-    pass
-
-
-if os.environ.get('RULESFILE'):
-    try:
-        print("Cargando JSON file", os.environ['RULESFILE'])
-
-        with open(os.environ['RULESFILE'], 'r') as jsonf:
-            jsonData = json.loads(jsonf.read())
-            rules = jsonData['rules']
-
-            if not convertionTable:
-                convertionTable = jsonData['convertionTable']
-    except IOError:
-        raise JSONReadError('Error reading JSON file.')
+if rulesfile and os.path.exists(rulesfile):
+    jsonData = loadRulesFromFile(rulesfile)
+    rules = jsonData['rules']
+    convertionTable = jsonData['convertionTable']
+else:
+    rules = {}
+    convertionTable = {}
 
 
 class Freeway(object):
-    def __init__(self, filepath=None, pattern='auto', rules=rules,
+    def __init__(self, filepath=None, pattern=['auto'], rules=rules,
                  convertionTable=convertionTable, rulesfile=None, **kwargs):
-        self._rules = {}
 
-        if not rulesfile:
-            rulesfile = os.environ['RULESFILE']
+        
 
-        if not rules and os.environ.get('RULESFILE'):
-            try:
-                # print "Cargando JSON file", rulesfile
-                with open(rulesfile, 'r') as jsonf:
-                    jsonData = json.loads(jsonf.read())
-                    rules = jsonData['rules']
-
-                    if not convertionTable:
-                        convertionTable = jsonData['convertionTable']
-
-            except IOError:
-                raise JSONReadError('Error reading JSON file.')
-
-        if isinstance(pattern, basestring):
-            pattern = [pattern]
-        self.pattern = pattern
-
-        self._rules = OrderedDict(Freeway.get_rules(rules))
-        self._convertionTable = convertionTable
+        if rulesfile:
+            self._rulesfile = rulesfile
+            jsonData = loadRulesFromFile(rulesfile)
+            self._rules = OrderedDict(Freeway.get_rules(jsonData['rules']))
+            self._convertionTable = jsonData['convertionTable']
+        else:
+            self._rules = OrderedDict(Freeway.get_rules(rules))
+            self._convertionTable = convertionTable
 
         if filepath:
-            self.parseFilepath(filepath, self.pattern)
+            self._filepath = filepath.replace('\\', '/')
+            self.pattern = pattern
 
         for key, value in kwargs.items():
             setattr(self, key, value)
 
     def __str__(self):
-        return str(self.data)
+        return '%s: %s' % (self.pattern, str(self.data))
 
     def __repr__(self):
         return str(self)
 
     def parseFilepath(self, filepath, patterns=['auto']):
         if filepath:
-            self.filepath = filepath.replace('\\', '/')
-
-            for key, value in Freeway.info_from_path(self.filepath,
+            for key, value in Freeway.info_from_path(filepath,
                                                      self._rules,
                                                      self.pattern).items():
                 setattr(self, key, value)
 
     @staticmethod
     def info_from_path(path, rules, patterns=['auto']):
+        assert isinstance(path, str), 'Path isnt str type'
         for pattern in patterns:
             if pattern == 'auto':
                 for key, rule in rules.items():
@@ -97,7 +78,6 @@ class Freeway(object):
                         info = {key:value for key, value in match.groupdict().items() if not key.endswith('_')}
                         info['pattern'] = pattern
                         return info
-
             else:
                 for item in Freeway.expandRules(pattern, Freeway(rules=rules)):
                     match = re.match(item.regex, path, re.IGNORECASE)
@@ -130,19 +110,21 @@ class Freeway(object):
         return fullMatch
 
     @property
-    def dirname(self):
-        return os.path.dirname(self.filepath)
+    def pattern(self):
+        return self._pattern
 
-    @property
-    def basename(self):
-        return os.path.basename(self.filepath)
+    @pattern.setter
+    def pattern(self, value):
+        self._pattern = [value] if isinstance(value, str) else value
+        self.parseFilepath(self._filepath, self._pattern)
 
     @property
     def data(self):
         elements = self.__dict__.copy()
-        elements.pop('_rules')
-        elements.pop('_convertionTable')
-        elements.pop('pattern')
+        for attr in ['pattern', '_filepath', '_rules',
+                     '_convertionTable', '_rulesfile']:
+            elements.pop(attr, None)
+ 
         return elements
 
     @staticmethod
@@ -156,19 +138,14 @@ class Freeway(object):
         rules._rules["_ignoreMissing"] = True
 
         for rule in rules._rules.get(attr, []):
-            antiloop = 0
-            rule = RuleParser(attr, unicode(rule))
+            rule = RuleParser(attr, str(rule))
             field = rule
 
             while set(rules._rules) & set(rule.fields):
                 for field in rule.fields:
                     if field in rules:
                         rule.rule = rule.rule.replace('{%s}' % field,
-                                                      rules.get(field, field))
-
-                if antiloop > 10000:
-                    raise Exception('Loop infinito en Freeway.expandRules(), variable> %s' % field)
-                antiloop += 1
+                                                    rules.get(field, field))
 
             yield rule
 
@@ -198,7 +175,7 @@ class Freeway(object):
             missing = [part for part in _PLACEHOLDER_REGEX.findall(name)]
 
             if missing and not ignoreMissing:
-                raise NoAttributes('No se ha encontrado el atributo: %s' % ', '.join(missing))
+                raise AttributeError('No se ha encontrado el atributo: %s' % ', '.join(missing))
 
             return name
         else:
@@ -219,10 +196,7 @@ class Freeway(object):
                                 pass
 
     def __contains__(self, item):
-        for rule in self._rules:
-            if rule == item:
-                return True
-        return False
+        return any(filter(lambda x: x == item, self._rules))
 
     def __getitem__(self, item):
         for rule in self._rules:
@@ -238,15 +212,15 @@ class Freeway(object):
         self.__dict__.update(data)
 
     def clean(self):
-        for key, value in self.__dict__.items():
-            if key not in ['pattern', '_rules', '_convertionTable']:
-                delattr(self, key)
+        notRemove = ['pattern', '_rules', '_convertionTable', '_rulesfile']
+        for key in set(self.__dict__) ^ set(notRemove):
+            self.__dict__.pop(key, None)
 
 
 class RuleParser(object):
     def __init__(self, name, rule):
         self.name = name
-        self.rule = unicode(rule)
+        self.rule = str(rule)
 
     def __getitem__(self, item):
         index = 0
@@ -302,14 +276,9 @@ class RuleParser(object):
         return '' + regexRule
 
 
-class NoAttributes(Exception):
-    pass
-
-
 if __name__ == '__main__':
-    ruta = r"C_Flower1_mesh|C_Flower1_meshShape"
-    myPath = Freeway(ruta, pattern=['meshName','instanceMeshName'])
-    print(myPath)
     ruta = r"C_Flower1_meshShape"
     myPath = Freeway(ruta, pattern=['meshName','instanceMeshName'])
+    print(myPath)
+    myPath.clean()
     print(myPath)
