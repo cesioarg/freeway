e38237ecaabc03c191e2c6b7a0f2b999fc08f8c8
Leandro Inocencio <cesio.arg@gmail.com>
2020-09-07 22:26:59 -0500

    Initial structure for testing freeway

---
+++ tests/test_freeway.py
@@ -0,0 +1,71 @@
+import unittest
+from pathlib import Path
+from versioner import Version
+
+
+class Test_RuleParser(unittest.TestCase):
+    def test___getitem__(self, item):
+        pass
+    
+    def test___contains__(self, item):
+        pass
+    
+    def test_lenFields(self):
+        pass
+    
+    def test_fields(self):
+        pass
+    
+    def test_regex(self):
+        pass
+
+
+class Test_Freeway(unittest.TestCase):
+    def Test_parseFilepath(self, filepath, patterns=['auto']):
+        pass
+    
+    def Test_info_from_path(path, rules, patterns=['auto']):
+        pass
+    
+    def Test_match(self):
+        pass
+    
+    def Test_getter_pattern(self):
+        pass
+    
+    def Test_setter_pattern(self, value):
+        pass
+    
+    def Test_data(self):
+        pass
+    
+    def Test_get_rules(allrules):
+        pass
+    
+    def Test_expandRules(attr, rules):
+        pass
+    
+    def Test___getattribute__(self, attr):
+        pass
+    
+    def Test___contains__(self, item):
+        pass
+    
+    def Test___getitem__(self, item):
+        pass
+    
+    def Test_get(self, item, default=None):
+        pass
+    
+    def Test_update(self, data):
+        pass
+    
+    def Test_clean(self):
+        pass
+    
+    def Test_version(self, attr):
+        pass
+    
+
+if __name__ == '__main__':
+    unittest.main()
