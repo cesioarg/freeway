daf3ccdf5294ce23ea0271745e2c8ef19e8e36d3
Leandro Inocencio <cesio.arg@gmail.com>
2020-10-16 20:10:02 -0500

    Add some examples

--- src/main.py
+++ src/main.py
@@ -2,6 +2,44 @@ from freeway import Freeway
 
 
 if __name__ == '__main__':
-    myPath = Freeway(rules={"root": ["{asset}.v{version}.{ext}"]}, pattern="root")
-    myPath.filepath = r"Z:/projects/assets/asset.v007.abc"
-    print(myPath.data)
+    # Parse data from path
+    filepath = r"C:/example/assets/Characters/Roberto/MOD/Work/example_CH_Roberto_MOD.v001.abc"
+    myPath = Freeway(filepath)
+    # Show all parsed data
+    print(myPath)
+
+    # Use parsed data
+    print("%s_%s_%s_example" % (myPath.asset, myPath.assetType, myPath.task))
+
+    # Use parsed data for make new paths
+    print(myPath.assetDir)
+    
+    # Make a path from data
+    data = {'assetType': 'Prop',
+            'asset': 'Table',
+            'process': 'MOD',
+            'stage': 'Work',
+            'assetPrefix': 'PR',
+            'task': 'MOD',
+            'version': '001',
+            'ext': 'abc'}
+
+    myPath = Freeway(**data)
+
+    print(myPath.assetWorkspacePath)
+    print(myPath.assetFile)
+    print(myPath.assetDir)
+
+    # Modify parsed data to make new paths
+    other = myPath.copy()
+    myPath.stage = "Publish"
+    myPath.ext = "usd"
+    myPath.asset = "Chair"
+    print(myPath.assetWorkspacePath)
+
+    # Or also
+    other.update({"process": "SHD",
+                  "ext": "mb",
+                  "version": "123"})
+
+    print(other.assetWorkspacePath)
