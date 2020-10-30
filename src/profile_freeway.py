273550863232777edfb69f4076b6bf7fe8f228f5
Leandro Inocencio <cesio.arg@gmail.com>
2020-10-30 14:54:24 -0500

    Add cProfile tests

---
+++ src/profile_freeway.py
@@ -0,0 +1,30 @@
+from __future__ import print_function
+from cProfile import run
+from freeway import Freeway
+
+
+def test1(cycles=10000):
+    filepath = r"C:/example/assets/Characters/Roberto/MOD/Work/example_CH_Roberto_MOD.v001.abc"
+    # Parse data from path
+    for cycle in range(cycles):
+        myPath = Freeway(filepath)
+        myPath.asset = 'Tito'
+    # Use parsed data for make new paths
+    print("Result:", myPath.assetDir)
+
+
+def test2(cycles=50000):
+    # Parse data from path
+    filepath = r"C:/example/assets/Characters/Roberto/MOD/Work/example_CH_Roberto_MOD.v001.abc"
+    myPath = Freeway(filepath)
+    for cycle in range(cycles):
+        myPath.asset = 'Tito'
+        data = myPath.assetDir
+        
+
+    # Use parsed data for make new paths
+    print("Result:", myPath.assetDir)
+
+
+if __name__ == '__main__':
+    run("test2()", sort="time")
